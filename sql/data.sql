0619d3b0cd216c435c4c7a623c068e5d2bdcb5b4
Ryan Carter <ryan@cloudflare.com>
2016-01-23 13:16:37 -0800

    move annotation table data to data.sql

---
+++ sql/data.sql
@@ -0,0 +1,20 @@
+--
+-- Data for Name: annotation; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY annotation (short_name, long_name) FROM stdin;
+!H      host unreachable
+!N      network unreachable
+!P      protocol unreachable
+!S      source route failed
+!F      fragmentation needed
+!X      communication administratively prohibited
+!V      host precedence violation
+!C      precedence  cutoff in effect
+!8      source host isolated
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
